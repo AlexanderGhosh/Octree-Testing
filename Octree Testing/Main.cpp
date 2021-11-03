c9a1133307e09d8f719aea913abdf0e38981da96
AlexanderGhosh <ghoshalexander@gmail.com>
2021-11-03 15:46:36 +0000

    started opengl

--- Octree Testing/Main.cpp
+++ Octree Testing/Main.cpp
@@ -1,4 +1,13 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+
+#include <glm.hpp>
+#include <gtc/matrix_transform.hpp>
+
 #include <iostream>
+#include <fstream>
+#include <sstream>
 #include <list>
 
 #include "Timer.h"
@@ -12,11 +21,16 @@
 #define WORLD_SPACE_Z 5
 
 #define MAX_RECERSIVE_DEPTH 10
-#define MAX_ITTERATIONS 1000000
+#define MAX_ITTERATIONS 1
+
+constexpr glm::ivec2 DIMENTIONS(800, 400);
+constexpr glm::fvec3 BG_COLOUR(0.5, 0.75, 0.5);
 
 using namespace std;
 
 list<Octree> trees;
+GLFWwindow* window;
+unsigned VBO, VAO;
 
 float randRange(float min, float max) {
 	return min + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (max - min)));
@@ -129,6 +143,13 @@ void BuildTree(Octree* node) {
 	
 }
 
+
+void initOpenGL(GLFWwindow*& window);
+void closeOpenGL();
+int initShader();
+void initBuffers();
+void destroyBuffers();
+
 void main() {
 	srand(0);
 	vector<Vec3> objects;
@@ -163,7 +184,11 @@ void main() {
 	for (int i = 0; i < MAX_ITTERATIONS; i++)
 	{
 		timer.start();
+
+
 		BuildTree(root);
+
+
 		Octree::idCounter = 1;
 		trees.clear();
 		trees.emplace_back();
@@ -177,4 +202,185 @@ void main() {
 	}
 	total /= (float)MAX_ITTERATIONS;
 	cout << to_string(total);
+
+	glm::vec3 viewPos(0, 0, -10), fwd(0, 0 ,-1);
+	glm::mat4 view(1), proj(1);
+
+	view = glm::lookAt(viewPos, viewPos + fwd, { 0, 1, 0 });
+	proj = glm::perspective<float>(glm::radians(45.0f), (float)DIMENTIONS.x / (float)DIMENTIONS.y, 0.1, 1000);
+
+	initOpenGL(window);
+	initBuffers();
+	glPointSize(5);
+	const int shader = initShader();
+
+	while (!glfwWindowShouldClose(window)) {
+		glClearColor(BG_COLOUR.x, BG_COLOUR.y, BG_COLOUR.z, 1);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		
+		glUseProgram(shader);
+
+		const float radius = 5.0f;
+		float camX = sin(glfwGetTime()) * radius;
+		float camZ = cos(glfwGetTime()) * radius;
+		camX = 0;
+		camZ = 5;
+		glm::mat4 view;
+		view = glm::lookAt(glm::vec3(camX, 0.0, camZ), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 1.0, 0.0));
+		// view = glm::lookAt(viewPos, viewPos + fwd, { 0, 1, 0 });
+
+		int loc = glGetUniformLocation(shader, "view");
+		glUniformMatrix4fv(loc, 1, GL_TRUE, &view[0][0]);
+
+		loc = glGetUniformLocation(shader, "proj");
+		glUniformMatrix4fv(loc, 1, GL_TRUE, &proj[0][0]);
+
+		glm::mat4 model(1);
+		model = glm::translate(model, { 0, 0, 0 });
+		loc = glGetUniformLocation(shader, "model");
+		glUniformMatrix4fv(loc, 1, GL_TRUE, &model[0][0]);
+
+		glBindVertexArray(VAO);
+
+		glDrawArrays(GL_POINTS, 0, 3);
+
+		glfwSwapInterval(0);
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	destroyBuffers();
+	closeOpenGL();
+}
+
+void initOpenGL(GLFWwindow*& win) {
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	// glfwWindowHint(GLFW_SAMPLES, 4);
+	// glfwSetErrorCallback(GLFWErrorCallBack);
+	win = glfwCreateWindow(DIMENTIONS.x, DIMENTIONS.y, "Cellular Automation", NULL, NULL);
+	if (!win)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return;
+	}
+	glfwMakeContextCurrent(win);
+
+	// glfwSetInputMode(win, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+	glViewport(0, 0, DIMENTIONS.x, DIMENTIONS.y);
+	glewInit();
+
+	// glfwSetKeyCallback(window, keyCallBack);
+}
+
+void closeOpenGL()
+{
+	glfwMakeContextCurrent(nullptr);
+	glfwDestroyWindow(window);
+	window = nullptr;
+	glfwTerminate();
+}
+
+
+int initShader()
+{
+	const std::string filePath = "Shader";
+	const std::string extensions[] = { "/vert.glsl", "/frag.glsl" };
+	std::string codes[] = { "", "", "" };
+	std::ifstream streams[] = { { }, { }, { } };
+	const GLenum types[] = { GL_VERTEX_SHADER, GL_FRAGMENT_SHADER };
+	unsigned shaders[] = { 0, 0, 0 };
+
+	int success;
+	char infoLog[512];
+	for (short i = 0; i < 2; i++) {
+		const std::string& extension = extensions[i];
+		std::string& code = codes[i];
+		std::ifstream& stream = streams[i];
+		const GLenum& type = types[i];
+		unsigned& shader = shaders[i];
+
+		stream.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+		try
+		{
+			// open files
+			stream.open(filePath + extension);
+			std::stringstream s;
+			// read file's buffer contents into streams
+			s << stream.rdbuf();
+			// close file handlers
+			stream.close();
+			// convert stream into string
+			code = s.str();
+		}
+		catch (std::ifstream::failure e)
+		{
+			std::cout << filePath + extension << " not read" << std::endl;
+		}
+		const char* c = code.c_str();
+
+		shader = glCreateShader(type);
+		glShaderSource(shader, 1, &c, NULL);
+		glCompileShader(shader);
+
+		glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+		if (!success)
+		{
+			glGetShaderInfoLog(shader, 512, NULL, infoLog);
+			std::cout << filePath + extension << " faild to compile " << infoLog << std::endl;
+		}
+	}
+
+	// link shaders
+	int shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, shaders[0]); // vertex
+	glAttachShader(shaderProgram, shaders[1]); // fragment
+	glLinkProgram(shaderProgram);
+	// check for linking errors
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << filePath << " faild to link " << infoLog << std::endl;
+	}
+
+	glDetachShader(shaderProgram, shaders[0]);
+	glDetachShader(shaderProgram, shaders[1]);
+
+	glDeleteShader(shaders[0]);
+	glDeleteShader(shaders[1]);
+
+	glUseProgram(0);
+	return shaderProgram;
+}
+
+void destroyBuffers()
+{
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+}
+
+
+void initBuffers()
+{
+	std::vector<float> data{
+		 -1, -1,  -1,
+		 -0.5, -0.5,  -0.5,
+		 0, 0, 0
+	};
+
+	glGenBuffers(1, &VBO);
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * data.size(), &data[0], GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(glm::vec3), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
 }
